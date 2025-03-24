d18bb52789c07238b40ab591c63cf86dd86f865f
Dima <dmitry.usov@yahoo.com>
2025-03-24 11:05:01 +0300

    fix: update branches for validation on pr
    * fix: set branches to main and feature/ci-cd
    
    * fix: remove typo in script
    
    * fix: change workflow
    
    * fix: change way of value share
    
    * fix: typo
    
    * refactor: remove redundant code

--- .github/scripts/validate-branch-name.sh
+++ .github/scripts/validate-branch-name.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 BRANCH_NAME=${1:-$(git rev-parse --abbrev-ref HEAD)}
-REGEX_PATTERN= '^feature\/.+$'
+REGEX_PATTERN="^feature//*"
 
-if [["$BRANCH_NAME" =~ $REGEX_PATTERN]]; then
+if [[ "$BRANCH_NAME" =~ $REGEX_PATTERN ]]; then
   echo "Branch name '$BRANCH_NAME' is valid."
   exit 0
 else
